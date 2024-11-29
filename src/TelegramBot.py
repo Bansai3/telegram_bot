5e72ace7e418a126112d1de7e8621555a64d8475
GoldenGarr <GoldenGarr@...>
2024-11-29 22:24:37 +0300

    refactor: bot add: testing

--- src/TelegramBot.py
+++ src/TelegramBot.py
@@ -28,122 +28,137 @@ class TelegramBot:
         self.__vpn_service = VPNService(host)
         self.log = setup_logging()
 
+        bot_token = self.__get_bot_token()
+        self.bot = telebot.TeleBot(bot_token)
+
     def start(self):
         self.handle_button()
         self.log.info('Bot started successfully: %s', datetime.datetime.now())
 
-    def handle_button(self):
-        bot_token = self.__get_bot_token()
-        bot = telebot.TeleBot(bot_token)
+    def handle_info_command(self, message):
+        self.bot.send_message(message.chat.id, commands['info'](), parse_mode='html')
 
-        @bot.message_handler(commands=['start'])
-        def handle_start_command(message):
+    def handle_start_command(self, message):
+        try:
+            user_name = self.__user_service.get_user(message)
+            self.bot.send_message(message.chat.id, commands['start'](user_name), parse_mode='html')
+        except ServerErrorException as e:
+            self.bot.send_message(message.chat.id, commands['server_error'](), parse_mode='html')
+        except Exception as e:
+            print(str(e))
+            response_message = self.__user_service.register_user(message)
+            self.bot.send_message(message.chat.id, commands['start'](response_message), parse_mode='html')
+        # bot.send_message(message.chat.id, commands['start'](message.from_user.first_name + ' ' + message.from_user.last_name), parse_mode='html')
+
+    def handle_subscription_info_command(self, message):
+        self.bot.send_message(message.chat.id, commands['subscription_info'](), parse_mode='html')
+
+    def handle_payment_command(self, message):
+        markup_inline = types.InlineKeyboardMarkup()
+        item_subscription = types.InlineKeyboardButton(text='Купить подписку', callback_data='buy_sub')
+        markup_inline.add(item_subscription)
+        self.bot.send_message(message.chat.id,
+                              'Нажжмите "Купить подписку" если хотите приобрести пробную или платную подписку',
+                              reply_markup=markup_inline)
+
+    def subscription_handler(self, call):
+        if call.data == 'buy_sub':
             try:
-                user_name = self.__user_service.get_user(message)
-                bot.send_message(message.chat.id, commands['start'](user_name), parse_mode='html')
+                countries = self.__vpn_service.get_countries()
             except ServerErrorException as e:
-                bot.send_message(message.chat.id, commands['server_error'](), parse_mode='html')
+                self.bot.send_message(call.message.chat.id, str(e))
+                return
             except Exception as e:
-                print(str(e))
-                response_message = self.__user_service.register_user(message)
-                bot.send_message(message.chat.id, commands['start'](response_message), parse_mode='html')
-            # bot.send_message(message.chat.id, commands['start'](message.from_user.first_name + ' ' + message.from_user.last_name), parse_mode='html')
-
-        @bot.message_handler(commands=['info'])
-        def handle_info_command(message):
-            bot.send_message(message.chat.id, commands['info'](), parse_mode='html')
-
-        @bot.message_handler(commands=['subscription_info'])
-        def handle_subscription_info_command(message):
-            bot.send_message(message.chat.id, commands['subscription_info'](), parse_mode='html')
-
-        @bot.message_handler(commands=['subscription'])
-        def handle_payment_command(message):
+                self.bot.send_message(call.message.chat.id, str(e))
+                return
             markup_inline = types.InlineKeyboardMarkup()
-            item_subscription = types.InlineKeyboardButton(text='Купить подписку', callback_data='buy_sub')
-            markup_inline.add(item_subscription)
-            bot.send_message(message.chat.id,
-                             'Нажжмите "Купить подписку" если хотите приобрести пробную или платную подписку',
-                             reply_markup=markup_inline)
-
-        @bot.callback_query_handler(func=lambda call: True)
-        def subscription_handler(call):
-            if call.data == 'buy_sub':
-                try:
-                    countries = self.__vpn_service.get_countries()
-                except ServerErrorException as e:
-                    bot.send_message(call.message.chat.id, str(e))
-                    return
-                except Exception as e:
-                    bot.send_message(call.message.chat.id, str(e))
-                    return
+            for country in countries:
+                markup_inline.add(
+                    types.InlineKeyboardButton(text=country.name, callback_data=f'country_{country.name}'))
+            self.bot.send_message(call.message.chat.id, 'Выберите страну', reply_markup=markup_inline)
+        else:
+            command_parts = call.data.split('_')
+            if command_parts[0] == 'country':
                 markup_inline = types.InlineKeyboardMarkup()
-                for country in countries:
-                    markup_inline.add(
-                        types.InlineKeyboardButton(text=country.name, callback_data=f'country_{country.name}'))
-                bot.send_message(call.message.chat.id, 'Выберите страну', reply_markup=markup_inline)
-            else:
-                command_parts = call.data.split('_')
-                if command_parts[0] == 'country':
-                    markup_inline = types.InlineKeyboardMarkup()
-                    item_trial_subscription = types.InlineKeyboardButton(text='Пробная подписка',
-                                                                         callback_data=f'trial_sub_{call.data}')
-                    item_subscription = types.InlineKeyboardButton(text='Платная подписка',
-                                                                   callback_data=f'sub_{call.data}')
-                    markup_inline.add(item_trial_subscription, item_subscription)
-                    bot.send_message(call.message.chat.id, 'Выберите подписку', reply_markup=markup_inline)
-                elif command_parts[0] == 'trial':
-                    try:
-                        key_value = self.__get_key(call.data, call.from_user.id, True)
-                        bot.send_message(call.message.chat.id, commands['key'](key_value), parse_mode='html')
-                    except ServerErrorException as e:
-                        bot.send_message(call.message.chat.id, str(e))
-                    except:
-                        bot.send_message(call.message.chat.id, errors['trial_subscription_already_bought']())
-                elif command_parts[0] == 'sub':
-                    try:
-                        subscriptions_ids = self.__get_user_subscriptions(call.from_user.id)
-                        chat_member = bot.get_chat_member(self.__get_subscription_group_id(), call.from_user.id)
-                        if len(subscriptions_ids) == 0 and chat_member.status in ['member', 'administrator', 'creator']:
-                            bot.kick_chat_member(call.message.chat.id, call.from_user.id)
-                            bot.unban_chat_member(call.message.chat.id, call.from_user.id)
-                        elif len(subscriptions_ids) != 0 and chat_member.status in ['member', 'administrator',
-                                                                                    'creator']:
-                            bot.send_message(call.message.chat.id, errors['subscription_already_bought']())
-                            return
-                    except ServerErrorException as e:
-                        bot.send_message(call.message.chat.id, str(e))
+                item_trial_subscription = types.InlineKeyboardButton(text='Пробная подписка',
+                                                                     callback_data=f'trial_sub_{call.data}')
+                item_subscription = types.InlineKeyboardButton(text='Платная подписка',
+                                                               callback_data=f'sub_{call.data}')
+                markup_inline.add(item_trial_subscription, item_subscription)
+                self.bot.send_message(call.message.chat.id, 'Выберите подписку', reply_markup=markup_inline)
+            elif command_parts[0] == 'trial':
+                try:
+                    key_value = self.__get_key(call.data, call.from_user.id, True)
+                    self.bot.send_message(call.message.chat.id, commands['key'](key_value), parse_mode='html')
+                except ServerErrorException as e:
+                    self.bot.send_message(call.message.chat.id, str(e))
+                except:
+                    self.bot.send_message(call.message.chat.id, errors['trial_subscription_already_bought']())
+            elif command_parts[0] == 'sub':
+                try:
+                    subscriptions_ids = self.__get_user_subscriptions(call.from_user.id)
+                    chat_member = self.bot.get_chat_member(self.__get_subscription_group_id(), call.from_user.id)
+                    if len(subscriptions_ids) == 0 and chat_member.status in ['member', 'administrator', 'creator']:
+                        self.bot.kick_chat_member(call.message.chat.id, call.from_user.id)
+                        self.bot.unban_chat_member(call.message.chat.id, call.from_user.id)
+                    elif len(subscriptions_ids) != 0 and chat_member.status in ['member', 'administrator',
+                                                                                'creator']:
+                        self.bot.send_message(call.message.chat.id, errors['subscription_already_bought']())
                         return
-                    bot.send_message(call.message.chat.id, 'Для оплаты подписки перейдите по данной ссылке:\n\n' +
-                                     self.__get_subscription_link())
-                    markup_inline = types.InlineKeyboardMarkup()
-                    user_paid_sub = types.InlineKeyboardButton(text='Я оплатил',
-                                                               callback_data=f'paid_{call.data}')
-                    markup_inline.add(user_paid_sub)
-                    bot.send_message(call.message.chat.id, 'После оплаты нажмите "Я оплатил"',
-                                     reply_markup=markup_inline)
-                elif command_parts[0] == 'paid':
-                    try:
-                        chat_member = bot.get_chat_member(self.__get_subscription_group_id(), call.from_user.id)
-                        if chat_member.status in ['member', 'administrator', 'creator']:
-                            bot.send_message(call.message.chat.id, commands['paid_sub_info'](), parse_mode='html')
-                            self.log.info('User %s paid for subscription', call.from_user.id)
-                            self.log.debug('User info: %s', chat_member.user)
+                except ServerErrorException as e:
+                    self.bot.send_message(call.message.chat.id, str(e))
+                    return
+                self.bot.send_message(call.message.chat.id, 'Для оплаты подписки перейдите по данной ссылке:\n\n' +
+                                      self.__get_subscription_link())
+                markup_inline = types.InlineKeyboardMarkup()
+                user_paid_sub = types.InlineKeyboardButton(text='Я оплатил',
+                                                           callback_data=f'paid_{call.data}')
+                markup_inline.add(user_paid_sub)
+                self.bot.send_message(call.message.chat.id, 'После оплаты нажмите "Я оплатил"',
+                                      reply_markup=markup_inline)
+            elif command_parts[0] == 'paid':
+                try:
+                    chat_member = self.bot.get_chat_member(self.__get_subscription_group_id(), call.from_user.id)
+                    if chat_member.status in ['member', 'administrator', 'creator']:
+                        self.bot.send_message(call.message.chat.id, commands['paid_sub_info'](), parse_mode='html')
+                        self.log.info('User %s paid for subscription', call.from_user.id)
+                        self.log.debug('User info: %s', chat_member.user)
 
-                            key_value = self.__get_key(call.data, call.from_user.id, False)
-                            bot.send_message(call.message.chat.id, commands['key'](key_value), parse_mode='html')
-                        else:
-                            self.log.error('User %s is not a member of the subscription group', call.from_user.id)
-                            bot.send_message(call.message.chat.id, errors['not_found_after_payment_error'](),
-                                             parse_mode='html')
-                    except ServerErrorException as e:
-                        self.log.error('Error occurred while checking user membership in the subscription group: %s', e)
-                        bot.send_message(call.message.chat.id, str(e), parse_mode='html')
-                    except Exception as e:
-                        self.log.error('Error occurred while checking user membership in the subscription group: %s', e)
-                        bot.send_message(call.message.chat.id, errors['server_connection_error'], parse_mode='html')
+                        key_value = self.__get_key(call.data, call.from_user.id, False)
+                        self.bot.send_message(call.message.chat.id, commands['key'](key_value), parse_mode='html')
+                    else:
+                        self.log.error('User %s is not a member of the subscription group', call.from_user.id)
+                        self.bot.send_message(call.message.chat.id, errors['not_found_after_payment_error'](),
+                                              parse_mode='html')
+                except ServerErrorException as e:
+                    self.log.error('Error occurred while checking user membership in the subscription group: %s', e)
+                    self.bot.send_message(call.message.chat.id, str(e), parse_mode='html')
+                except Exception as e:
+                    self.log.error('Error occurred while checking user membership in the subscription group: %s', e)
+                    self.bot.send_message(call.message.chat.id, errors['server_connection_error'], parse_mode='html')
 
-        bot.polling(none_stop=True)
+    def handle_button(self):
+        @self.bot.message_handler(commands=['start'])
+        def handle_start_command(message):
+            self.handle_start_command(message)
+
+        @self.bot.message_handler(commands=['info'])
+        def handle_info_command(message):
+            return self.handle_info_command(message)
+
+        @self.bot.message_handler(commands=['subscription_info'])
+        def handle_subscription_info_command(message):
+            self.handle_subscription_info_command(message)
+
+        @self.bot.message_handler(commands=['subscription'])
+        def handle_payment_command(message):
+            self.handle_payment_command(message)
+
+        @self.bot.callback_query_handler(func=lambda call: True)
+        def subscription_handler(call):
+            self.subscription_handler(call)
+
+        self.bot.polling(none_stop=True)
 
     @staticmethod
     def __get_bot_token() -> str:
