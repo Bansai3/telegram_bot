b7f0cc48b4a1e766165fa9cc71e850e0335b1d38
Maxim Tcai <89833124+GoldenGarr@users.noreply.github.com>
2024-12-15 18:42:18 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,13 @@
+import os, sys
 from TelegramBot import TelegramBot
+
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'utils'))
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'pb2_module/subscription'))
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'pb2_module/user'))
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'pb2_module/proxy'))
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'pb2_module/country'))
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'pb2_module/key'))
+
 
 if __name__ == '__main__':
     tb = TelegramBot()
