2e5f82b2b73ce162197ca34ba860704cb46293f7
Bansai3 <931307@bk.ru>
2024-12-15 18:50:32 +0300

    feat: update proto files

--- src/proto_script.sh
+++ src/proto_script.sh
@@ -1,1 +1,5 @@
-python3 -m grpc_tools.protoc --proto_path=. --python_out=proto_python --grpc_python_out=proto_python proto/*.proto
+python3 -m grpc_tools.protoc --proto_path=./proto --python_out=pb2_module/country --grpc_python_out=pb2_module/country proto/country.proto
+python3 -m grpc_tools.protoc --proto_path=./proto --python_out=pb2_module/key --grpc_python_out=pb2_module/key proto/key.proto
+python3 -m grpc_tools.protoc --proto_path=./proto --python_out=pb2_module/proxy --grpc_python_out=pb2_module/proxy proto/proxy.proto
+python3 -m grpc_tools.protoc --proto_path=./proto --python_out=pb2_module/subscription --grpc_python_out=pb2_module/subscription proto/subscription.proto
+python3 -m grpc_tools.protoc --proto_path=./proto --python_out=pb2_module/user --grpc_python_out=pb2_module/user proto/user.proto
